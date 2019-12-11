2fd0d3e3db066c81c5143140f453c590360b2f9c
shihab <shihab.gsc10@gmail.com>
2019-12-11 16:54:04 +0600

    migrations added

---
+++ database/migrations/2019_12_11_160955_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username',30)->unique();
+            $table->string('email',60)->unique();
+            $table->string('password',80);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
