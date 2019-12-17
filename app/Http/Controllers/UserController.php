4041c827f772183e77f7e42f066a383344601bd5
shihab <shihab.gsc10@gmail.com>
2019-12-17 13:53:53 +0600

    bug fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,9 +7,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\JWTAuth;
 
 class UserController extends Controller
 {
+    /**
+     * @var \Tymon\JWTAuth\JWTAuth
+     */
+    protected $jwt;
+
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
     function  index(){
         try{
             $users = app('db')
@@ -74,16 +85,48 @@ class UserController extends Controller
         }
 
         var_dump( Auth::guard('api')->attempt( $request->only('email','password') ) );
-//
-//        die();
+            //
+            //        die();
 
-//        app('auth')->authenticate($request->all());
+            //        app('auth')->authenticate($request->all());
 
         $user = app('db')
             ->table('users')
             ->where('email',$request->input('email'))
             ->andWhere('password',$request->input('password'))
             ->first();
+    }
+
+    
+
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        try {
+
+            if (! $token = $this->jwt->attempt($request->only('email', 'password'))) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], 500);
+
+        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], 500);
+
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent' => $e->getMessage()], 500);
+
+        }
+
+        return response()->json(compact('token'));
     }
 
     function response($message,$data,$status=200){
